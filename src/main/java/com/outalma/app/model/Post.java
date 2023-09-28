fa7407f4a9b00f0181e39c1ccd40c1d0e02038e5
ethicode <papesow.digital@gmail.com>
2023-09-28 21:43:08 +0000

    first commit

--- src/main/java/com/outalma/app/model/Post.java
+++ src/main/java/com/outalma/app/model/Post.java
@@ -1,6 +1,7 @@
 package com.outalma.app.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -11,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -27,6 +29,9 @@ public class Post {
 	
 	@Column(nullable=false)
 	private String description;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "post")
+    private List<Image> images;
 	
 	@ManyToOne
     @JoinColumn(name="user_id", nullable=false)
